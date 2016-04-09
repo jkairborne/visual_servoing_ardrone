dae3803f356c7ef0c85d282f6734701e7c4d8cad
Jason <jk@airsensing.com>
2016-04-08 21:52:12 -0400

    Got listener working, albeit with occasional segfaults. Also added quat_to_yaw, and a launch file

--- src/listener.cpp
+++ src/listener.cpp
@@ -3,12 +3,20 @@
 #include "ar_track_alvar_msgs/AlvarMarkers.h"
 #include "std_msgs/Float64.h"
 #include "publ_subs/test.h"
-
-
+#include "geometry_msgs/Vector3.h"
+#include "geometry_msgs/Quaternion.h"
+#include "tf/transform_datatypes.h"
+/*
+#include "LinearMath/btMatrix3x3.h"
+*/
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
-ros::Publisher chatterx;
+ros::Publisher pubx;
+ros::Publisher puby;
+ros::Publisher pubz;
+ros::Publisher pubquat;
+
 ros::Subscriber sub;
 
 void chatterCallback(const ar_track_alvar_msgs::AlvarMarkers& msg)
@@ -25,11 +33,17 @@ ROS_INFO("I heard: [%f]", msg.markers[0].pose.pose.position.x);
 	ros::Publisher chatterz = nh.advertise<std_msgs::Float64>("pose_z", 1000);
 */
         std_msgs::Float64 msgx;
+        std_msgs::Float64 msgy;
+        std_msgs::Float64 msgz;
+        geometry_msgs::Quaternion msgquat;
 /*  
       std_msgs::Float64 msgy;
         std_msgs::Float64 msgz;
  */
         msgx.data = msg.markers[0].pose.pose.position.x;
+        msgy.data = msg.markers[0].pose.pose.position.y;
+        msgz.data = msg.markers[0].pose.pose.position.z;
+        msgquat = msg.markers[0].pose.pose.orientation;
 /*   
      msgy.data = msg.markers[0].pose.pose.position.y;
         msgz.data = msg.markers[0].pose.pose.position.z;
@@ -42,7 +56,10 @@ ROS_INFO("I heard: [%f]", msg.markers[0].pose.pose.position.x);
      * given as a template parameter to the advertise<>() call, as was done
      * in the constructor above.
      */
-       chatterx.publish(msgx);
+       pubx.publish(msgx);
+       puby.publish(msgy);
+       pubz.publish(msgz);
+       pubquat.publish(msgquat);
 /*
        chattery.publish(msgy);
        chatterz.publish(msgz);
@@ -90,7 +107,10 @@ int main(int argc, char **argv)
    * is the number of messages that will be buffered up before beginning to throw
    * away the oldest ones.
    */
-  chatterx = n.advertise<std_msgs::Float64>("/pose_x",1000);
+  pubx = n.advertise<std_msgs::Float64>("/pose_x",1000);
+  puby = n.advertise<std_msgs::Float64>("/pose_y",1000);
+  pubz = n.advertise<std_msgs::Float64>("/pose_z",1000);
+  pubquat = n.advertise<geometry_msgs::Quaternion>("/pose_quat",1000);
   sub = n.subscribe("/ar_pose_marker", 1000, chatterCallback);
 
   /**
