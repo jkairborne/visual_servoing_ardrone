e6dcec8174fd5466846ddf1943523a94a754f516
Jason <jk@airsensing.com>
2016-04-09 22:49:49 -0400

    a few more changes, tried to create a velocity mux for the keyboard inputs

--- src/combine_floats.cpp
+++ src/combine_floats.cpp
@@ -2,29 +2,29 @@
 #include <std_msgs/Float64.h>
 #include <geometry_msgs/Twist.h>
 
-geometry_msgs::Twist vs123456;
+geometry_msgs::Twist vs_output;
 
 
 void callbackx(const std_msgs::Float64& input1)
 {
-  vs123456.linear.x = input1.data;
+  vs_output.linear.x = input1.data;
 }
 
 
 void callbacky(const std_msgs::Float64& input2)
 {
-  vs123456.linear.y = input2.data;
+  vs_output.linear.y = input2.data;
 }
 
 void callbackz(const std_msgs::Float64& input3)
 {
-  vs123456.linear.z = input3.data;
+  vs_output.linear.z = input3.data;
 }
 
 
 void callbackyaw(const std_msgs::Float64& input4)
 {
-  vs123456.angular.z = input4.data;
+  vs_output.angular.z = input4.data;
 }
 
 
@@ -32,12 +32,12 @@ void callbackyaw(const std_msgs::Float64& input4)
 int main(int argc, char** argv)
 {
   
-  vs123456.linear.x = 0.0;
-  vs123456.linear.y = 0.0;
-  vs123456.linear.z = 0.0;
-  vs123456.angular.x = 0.1; // to prevent auto-hover mode
-  vs123456.angular.y = 0.1; // to prevent auto-hover mode
-  vs123456.angular.z = 0.0;
+  vs_output.linear.x = 0.0;
+  vs_output.linear.y = 0.0;
+  vs_output.linear.z = 0.0;
+  vs_output.angular.x = 0.1; // to prevent auto-hover mode
+  vs_output.angular.y = 0.1; // to prevent auto-hover mode
+  vs_output.angular.z = 0.0;
 
 
   ros::init(argc, argv, "combination_node");
@@ -50,13 +50,13 @@ int main(int argc, char** argv)
   ros::Subscriber subz;
   ros::Subscriber subyaw;
 
-  subx = nh.subscribe("/vs123456_x", 1000, callbackx);
-  suby = nh.subscribe("/vs123456_y", 1000, callbacky);
-  subz = nh.subscribe("/vs123456_z", 1000, callbackz);
-  subyaw = nh.subscribe("/yaw_angle", 1000, callbackyaw);
+  subx = nh.subscribe("vs_output_x", 1000, callbackx);
+  suby = nh.subscribe("vs_output_y", 1000, callbacky);
+  subz = nh.subscribe("vs_output_z", 1000, callbackz);
+  subyaw = nh.subscribe("yaw_angle", 1000, callbackyaw);
 
   ros::Publisher pub;
-  pub = nh.advertise<geometry_msgs::Twist>("/vs123456",1000);
+  pub = nh.advertise<geometry_msgs::Twist>("/vs_output",1000);
 
   ros::spin();
 
