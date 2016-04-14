0c112d32cdb11629953a46e407123090ab6331b8
pin_lyu <pin.lv@flight.utias.utoronto.ca>
2016-04-14 15:06:37 -0400

    Messed around with a lot of things. Nothing quite working, currently trying to work on testing.cpp

--- src/transform_listener.cpp
+++ src/transform_listener.cpp
@@ -2,94 +2,77 @@
 #include "std_msgs/String.h"
 #include "ar_track_alvar_msgs/AlvarMarkers.h"
 #include "std_msgs/Float64.h"
-#include "publ_subs/test.h"
-#include "geometry_msgs/Vector3.h"
 #include "geometry_msgs/Quaternion.h"
 #include "tf/transform_datatypes.h"
 #include "tf/transform_listener.h"
 
-ros::Publisher pubx;
-ros::Publisher puby;
-ros::Publisher pubz;
-ros::Publisher pubquat;
-ros::Subscriber sub;
+bool arpose = false;
+
+void publishitall()
+{
+  tf::TransformListener listener;
 
 tf::StampedTransform transform;
-std_msgs::Float64 ypos, xpos, zpos;
-tf::Quaternion tfquat;
-geometry_msgs::Quaternion quat;
-
-void chatterCallback(const ar_track_alvar_msgs::AlvarMarkers& msg)
-
-{
-if (msg.markers.size()==0)
-{
-	ROS_INFO("made it just past msg.markers.size");
-return;
-}
-try{
-  tf::TransformListener listener;
-        listener.lookupTransform("/ardrone_base_bottomcam", "/ar_marker_0",  
-        ros::Time(0), transform);
+    try{
+      listener.lookupTransform("/odom", "/ardrone_base_bottomcam",
+                               ros::Time(0), transform);
+        ROS_INFO("I made it past the lookuptransform");
     }
-   catch (tf::TransformException ex){
+    catch (tf::TransformException ex){
       ROS_ERROR("%s",ex.what());
       ros::Duration(1.0).sleep();
     }
 
+}//end publishitall
 
-        ypos.data = transform.getOrigin().y();
-        xpos.data = transform.getOrigin().x();
-        zpos.data = transform.getOrigin().z();
-        tfquat = transform.getRotation();
-        tf::quaternionTFToMsg(tfquat, quat); 
-        pubx.publish(xpos);
-       puby.publish(ypos);
-       pubz.publish(zpos);
-       pubquat.publish(quat);
-	ROS_INFO("x,y,z: %f %f %f",xpos.data,ypos.data,zpos.data);
-        ros::Rate loop_rate(10);
-       ros::spinOnce();
-        loop_rate.sleep();
-  }
-
+void arpose_callback(const ar_track_alvar_msgs::AlvarMarkers& msg) 
+{
+if (msg.markers.size()==0){
+	arpose == false;
+	ROS_INFO("setting arpose to false");
+	} //end if
+else{
+	arpose == true;
+	ROS_INFO("setting arpose to true");
+	publishitall();	
+	} //end else
+}//end arpose_callback
 
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "my_tf_listener");
 
-  ros::NodeHandle node;
-
-  
-  pubx = node.advertise<std_msgs::Float64>("/pose_x",1000);
-  puby = node.advertise<std_msgs::Float64>("/pose_y",1000);
-  pubz = node.advertise<std_msgs::Float64>("/pose_z",1000);
-  pubquat = node.advertise<geometry_msgs::Quaternion>("/pose_quat",1000);
-
-	sub = node.subscribe("/ar_pose_marker", 1000, chatterCallback);
+  ros::NodeHandle nh;
 
 
+  ros::Publisher cmdx = 
+    nh.advertise<std_msgs::Float64>("/pose_c", 10);
 
+  tf::TransformListener listener;
 
-/*
+  ros::Rate rate(10.0);
+  while (nh.ok()){
+  ros::Subscriber sub = nh.subscribe("/ar_pose_marker", 1, arpose_callback);
+    tf::StampedTransform transform;
     try{
-      listener.lookupTransform("/ar_marker_0", "/ardrone_base_bottomcam",  
+      listener.lookupTransform("/ardrone_base_bottomcam", "/ar_marker_0",  
                                ros::Time(0), transform);
-	std_msgs::Float64 ypos, xpos, zpos;
-	tf::Quaternion tfquat;
-	geometry_msgs::Quaternion quat;
-	ypos.data = transform.getOrigin().y();
-	xpos.data = transform.getOrigin().x();
-	zpos.data = transform.getOrigin().z();
-	tfquat = transform.getRotation();
-	tf::quaternionTFToMsg(tfquat, quat); 
-	ROS_INFO("I see the transform x is %f and the y is %f",xpos.data,ypos.data);
-	pubx.publish(xpos);
-       puby.publish(ypos);
-       pubz.publish(zpos);
-       pubquat.publish(quat);
+	ROS_INFO("I made it past the lookuptransform");
+    }
+    catch (tf::TransformException ex){
+      ROS_ERROR("%s",ex.what());
+      ros::Duration(1.0).sleep();
+    }
+    if (arpose == true){
+	ROS_INFO("We made it into the true loop");
+	} //end if
+    else{
+	ROS_INFO("We made it into the false loop");
+	} //end else
 
-*/  
-  ros::spin(); 
+	ROS_INFO("I made it to the end");
+    ros::spin();
+    rate.sleep();
+  }
   return 0;
 };
