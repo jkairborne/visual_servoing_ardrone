28c69add04568a89f78f6ff5aa05396a3f0c3175
Jason <jk@airsensing.com>
2016-05-04 12:14:18 -0400

    removed all files related to the ar_track_alvar detection. This is now meant to work only with the built-in ardrone tracking

--- src/tag_detect2.cpp
+++ src/tag_detect2.cpp
@@ -28,33 +28,42 @@ return;
         ypos1 = (msg.tags_yc[0]-500)/1000;
         msgyaw.data = msg.tags_orientation[0];
         zpos1 = msg.tags_distance[0];
-*/
+
 
         xpos0 = (msg.tags_xc[0]-500.0)/1000.0;
         ypos0 = (msg.tags_yc[0]-500.0)/1000.0;
         msgyaw.data = msg.tags_orientation[0];
         msgz.data = msg.tags_distance[0];
 
-	xpos1 = xpos0 * msgz.data;
-	ypos1 = ypos0 * msgz.data;
+	msgx.data = xpos0 * msgz.data;
+	msgy.data = ypos0 * msgz.data;
 
-	rotx = msg.rotX*M_PI/180;
-	roty = msg.rotY*M_PI/180;
-	rotz = msg.rotZ*M_PI/180;
+
+	rotx = msg.rotX*M_PI/180.0;
+	roty = msg.rotY*M_PI/180.0;
+	rotz = msg.rotZ*M_PI/180.0;
 	double derotx, deroty, derotz;
+	
+	ROS_INFO("%f %f %f", rotx, roty, rotz);
+*/
 /*
   msgx.data = (cos(rotz)*cos(roty))*xpos1 + (cos(rotz)*sin(roty)*sin(rotx)-sin(rotz)*cos(rotx))*ypos1 + (cos(rotz)*sin(roty)*cos(rotx)+sin(rotz)*sin(rotx))*zpos1;
   msgy.data = sin(rotz)*cos(roty)*xpos1 + (sin(rotz)*sin(roty)*sin(rotx)+cos(rotz)*cos(rotx))*ypos1 + (sin(rotz)*sin(roty)*cos(rotx)-cos(rotz)*sin(rotx))*zpos1;
   msgz.data = -sin(roty)*xpos1+cos(roty)*sin(rotx)*ypos1+cos(roty)*cos(rotx)*zpos1;	
 */
+/*
   msgx.data = (cos(rotz)*cos(roty))*xpos1 - cos(roty)*sin(rotz)*ypos1 + sin(roty)*zpos1;
   msgy.data =  (cos(rotx)*sin(rotz) + cos(rotz)*sin(roty)*sin(rotx))*xpos1 + (cos(rotz)*cos(rotx) - sin(rotz)*sin(roty)*sin(rotx))*ypos1 - cos(roty)*sin(rotx)*zpos1;
 
   msgz.data = (sin(rotz)*sin(rotx) - cos(rotz)*cos(rotx)*sin(roty))*xpos1 + (cos(rotz)*sin(rotx) + cos(rotx)*sin(rotz)*sin(roty))*ypos1 + cos(roty)*cos(rotx)*zpos1;
-/*
-	double random2 = cos(rotx);
-	ROS_INFO("%f %f %f", rotx, roty, random2); 
 */
+	msgx.data = cos(msg.rotX*M_PI/180.0);
+	msgy.data = cos(msg.rotY*M_PI/180.0);
+	msgz.data = msg.rotZ*M_PI/180.0;
+	msgyaw.data = msg.tags_orientation[0];
+
+	ROS_INFO("%f %f yaw %f picture %f", msgx.data, msgy.data, msgz.data, msgyaw.data);
+
         pubx.publish(msgx);
         puby.publish(msgy);
         pubyaw.publish(msgyaw);
@@ -67,13 +76,13 @@ return;
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "listener");
+  ros::init(argc, argv, "listener2");
   ros::NodeHandle n;
 
-  pubx = n.advertise<std_msgs::Float64>("/pose_x",1000);
-  puby = n.advertise<std_msgs::Float64>("/pose_y",1000);
-  pubyaw = n.advertise<std_msgs::Float64>("/pose_yaw",1000);
-  pubz = n.advertise<std_msgs::Float64>("/pose_z",1000);
+  pubx = n.advertise<std_msgs::Float64>("/pose_x2",1000);
+  puby = n.advertise<std_msgs::Float64>("/pose_y2",1000);
+  pubyaw = n.advertise<std_msgs::Float64>("/pose_yaw2",1000);
+  pubz = n.advertise<std_msgs::Float64>("/pose_z2",1000);
 
   sub = n.subscribe("/ardrone/navdata", 1000, chatterCallback);
 
