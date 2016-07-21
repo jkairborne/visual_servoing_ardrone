48309c8e73e5336e5a7f3047d9fdcf5f1bb765fa
Borren <borrenmoe@gmail.com>
2016-07-21 12:31:54 -0400

    fixed inactive node thing

--- src/roomba_vel.cpp
+++ src/roomba_vel.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
+#include "std_msgs/Float64.h"
 #include <math.h>
 #include <sstream>
 
@@ -9,7 +10,8 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  ros::Publisher cmd2 = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1000);
+  ros::Publisher cmd_to_roomba = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1000);
+  ros::Publisher cmdxvel = n.advertise<std_msgs::Float64>("/roomba_vel_x", 1000);
 
   ros::Rate loop_rate(100);
 
@@ -17,11 +19,13 @@ int main(int argc, char **argv)
   while (ros::ok())
   {
     geometry_msgs::Twist cmdvel;
+    std_msgs::Float64 xvel_control;
 //    float cmdx = 1*sin(count/100.0);
     float cmdx = 0.5*sin(count/100.0);
     float cmdz = 0.000;
     cmdvel.linear.x = cmdx;
     cmdvel.angular.z = cmdz;
+    xvel_control.data = cmdx;
 //    cmdvel.header.stamp = ros::Time::now();
 //    cmdvel.header.stamp.nsec = count;
 //    cmdvel.header.stamp.sec = count;  
@@ -30,7 +34,8 @@ int main(int argc, char **argv)
 
     ROS_INFO("we have received %f and %f, %i", cmdvel.linear.x, cmdvel.angular.z, count);
 
-    cmd2.publish(cmdvel);
+    cmd_to_roomba.publish(cmdvel);
+    cmdxvel.publish(xvel_control);
     ros::spinOnce();
 
     loop_rate.sleep();
